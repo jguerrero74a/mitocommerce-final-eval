034945c5606299443d2236ebef9b1a23b9bf1ddd
jguerrero74a <jguerrero74@gmail.com>
2026-04-09 21:13:29 -0500

    feat: metodo de pago default

--- src/app/modules/checkout/pages/checkout-page/checkout-page.ts
+++ src/app/modules/checkout/pages/checkout-page/checkout-page.ts
@@ -12,15 +12,19 @@ import { CartSummary } from '../../components/cart-summary/cart-summary';
 export class CheckoutPage {
   private readonly store = inject(Store);
   public selectedShippingCost = signal<number>(0);
+  public selectedPaymentMethod = signal<string>('pago-contra-entrega');
 
   onShippingChange(cost: number): void {
     this.selectedShippingCost.set(cost);
   }
-
+  onPaymentChange(method: string): void {
+    this.selectedPaymentMethod.set(method);
+  }
   onSubmitOrder(): void {
     console.log('Pedido realizado con costo de envío:', this.selectedShippingCost());
     this.store.dispatch(clearCart());
     this.selectedShippingCost.set(0);
+    this.selectedPaymentMethod.set('pago-contra-entrega');
     alert('¡Pedido realizado y carrito vaciado!');
   }
 }
